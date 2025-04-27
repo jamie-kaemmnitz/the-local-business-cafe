1548aeca2fea9d23218233d3255cce0ff1b063db
jamie-kaemmnitz <jamiekaemmnitz24@gmail.com>
2025-04-27 13:31:28 +0100

    attempt to connect contact form and enquiries pages

--- java/main.js
+++ java/main.js
@@ -1,33 +1,40 @@
 const contactForm = document.getElementById('enquiry-form');
 
-contactForm.addEventListener('submit', function(e) {
-    e.preventDefault();
+if (contactForm) {
 
-    const contactFormData = new FormData(contactForm);
+    contactForm.addEventListener('submit', function(e) {
+        e.preventDefault();
 
-    const formData = {};
+        const contactFormData = new FormData(contactForm);
 
-    contactFormData.forEach((value, key) => {
-        formData[key] = value;
+        const formData = {};
+
+        contactFormData.forEach((value, key) => {
+            formData[key] = value;
+        });
+
+        localStorage.setItem('enquiryData', JSON.stringify(formData));
+
+        alert('Thank you for your enquiry, we will respond to you as soon as possible');
+        contactForm.reset();
     });
+};
 
-    localStorage.setItem('enquiryData', JSON.stringify(formData));
+const enquiryDetails = document.getElementById('enquiry-details');
 
-    alert('Thank you for your enquiry, we will respond to you as soon as possible');
-    contactForm.reset();
-});
+if (enquiryDetails) {
 
-const enquiryDataString = localStorage.getItem('enquiryData');
+    const enquiryDataString = localStorage.getItem('enquiryData');
 
-if (enquiryDataString) {
-    const enquiryData = JSON.parse(enquiryDataString);
-    const enquiryDetails = document.getElementById('enquiry-details');
+    if (enquiryDataString) {
+        const enquiryData = JSON.parse(enquiryDataString);
 
-    const output = '<ul>' + 
-        Object.keys(enquiryData).map(key => `<li><strong>${key}:</strong> ${enquiryData[key]}</li>`).join('')
-        + '</ul>';
+        const output = '<ul>' + 
+            Object.keys(enquiryData).map(key => `<li><strong>${key}:</strong> ${enquiryData[key]}</li>`).join('')
+            + '</ul>';
 
-    enquiryDetails.innerHTML = output;
+        enquiryDetails.innerHTML = output;
     } else {
-        document.getElementById('enquiry-details').innerHTML = '<p>No current enquiries</p>';
+        enquiryDetails.innerHTML = '<p>No current enquiries</p>';
+    };
 };
